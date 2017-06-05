ecdd50f520c83ee7e6602127e1e11b494e8100a0
rhn <gihu.rhn@porcupinefactory.org>
2017-06-05 14:40:06 +0200

    License: changed to dual GPLv3/EPLv1

--- gpx-rust/src/gpx/par.rs
+++ gpx-rust/src/gpx/par.rs
@@ -1,6 +1,15 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and the GNU General Public License
+ * v3.0 or later which accompanies this distribution.
+ * 
+ *      The Eclipse Public License (EPL) v1.0 is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * You may elect to redistribute this code under either of these licenses.     
+ */
 
 //! Parsing of GPX files.
 //!
